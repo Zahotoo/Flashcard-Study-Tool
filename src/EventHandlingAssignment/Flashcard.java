4b11202eed71efba63b7845c5e1ead7b8a0705be
Zahotoo <Zjh050330@gmail.com>
2025-12-01 04:52:30 +0000

    Done class Flashcard

--- src/EventHandlingAssignment/Flashcard.java
+++ src/EventHandlingAssignment/Flashcard.java
@@ -1,4 +1,21 @@
 package EventHandlingAssignment;
 
 public class Flashcard {
+
+    private String front;  // English
+    private String back;   // Translation
+
+    public Flashcard(String front, String back) {
+        this.front = front;
+        this.back = back;
+    }
+
+    public String getFront() {
+        return front;
+    }
+
+    public String getBack() {
+        return back;
+    }
 }
+
